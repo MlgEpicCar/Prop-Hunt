f1d3f49d9a1ccd0ae3513e9e012f1dd02af7286f
MlgEpicCar <115513939+MlgEpicCar@users.noreply.github.com>
2024-09-17 14:02:24 -0400

    Initial commit

---
+++ tile.py
@@ -0,0 +1,15 @@
+import pygame 
+from settings import *
+
+class Tile(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('Sprites/rock.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
+		self.hitbox = self.rect.inflate(0,-10)
+
+class Water(pygame.sprite.Sprite):
+	def __init__(self,pos,groups):
+		super().__init__(groups)
+		self.image = pygame.image.load('Sprites/watermelon.png').convert_alpha()
+		self.rect = self.image.get_rect(topleft = pos)
